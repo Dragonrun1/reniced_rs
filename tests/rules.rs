8d375a0efe7b4fadee0073ca9abc424a4494eba3
Michael Cummings <mgcummings@yahoo.com>
2026-05-27 10:20:28 -0700

    Simplify tests by refactoring `parse_rule` call and ensuring consistent file formatting

--- tests/rules.rs
+++ tests/rules.rs
@@ -3,10 +3,7 @@ use reniced::config::parse_rule;
 
 #[test]
 fn parses_combined_rule() -> Result<()> {
-    let rule = parse_rule(
-        "n-10r4o5",
-        "^seti",
-    )?;
+    let rule = parse_rule("n-10r4o5", "^seti")?;
 
     assert_eq!(rule.nice, Some(-10));
     assert_eq!(rule.oom_adj, Some(5));
